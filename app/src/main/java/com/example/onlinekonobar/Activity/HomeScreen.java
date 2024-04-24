0d2050ad94b4e467c31c4f9f4178261d3b75be29
lmMOulis <leon.moulis@gmail.com>
2024-04-24 07:54:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/onlinekonobar/Activity/HomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.onlinekonobar.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onlinekonobar.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
